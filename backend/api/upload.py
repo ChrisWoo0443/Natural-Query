9d319cb6d104d3c40b103948bc3dc775b8762658
ChrisWoo0443 <chefwoodini@gmail.com>
2025-05-04 23:39:12 -0700

    upload file post router

--- backend/api/upload.py
+++ backend/api/upload.py
@@ -4,9 +4,24 @@ import os
 
 router = APIRouter()
 
+'''
+Directory to save uploaded files
+when moving to cloud, change
+'''
+SAVE_DIR = "./temp"
+os.makedirs(SAVE_DIR, exist_ok=True)
+
 @router.post("/")
 async def upload_file(file: UploadFile):
-    file_location = f"data/{file.filename}"
-    with open(file_location, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-    return {"message": f"File saved at {file_location}"}
+    try:
+        file_path = os.path.join(SAVE_DIR, file.filename)
+
+        contents = await file.read()
+
+        with open(file_path, "wb") as f:
+            f.write(contents)
+            
+        return {"message": "File saved successfully"}
+    except Exception as e:
+        return {"message": e.args}
+    
